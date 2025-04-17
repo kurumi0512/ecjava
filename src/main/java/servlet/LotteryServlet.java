4da7a394fd18d2ed8d6cf70d879569c562a07b62
kurumimi <foreverlove0512t@gmail.com>
2025-04-17 16:10:23 +0800

    1610

--- src/main/java/servlet/LotteryServlet.java
+++ src/main/java/servlet/LotteryServlet.java
@@ -5,6 +5,7 @@ import java.util.LinkedHashSet;
 import java.util.Random;
 import java.util.Set;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,16 +16,19 @@ public class LotteryServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//1.生成樂透號碼
 		Random random = new Random();
 		Set<Integer>numbers = new LinkedHashSet<>();
 		while(numbers.size()<5) {
 			int number =random.nextInt(39)+1;
 			numbers.add(number);
 		}
-		resp.getWriter().print(numbers);
+		//2.生成調度器(分派器)
+		RequestDispatcher rd = req.getRequestDispatcher("/lottery2.jsp");
+		//3.傳遞樂透號碼
+		req.setAttribute("numbers", numbers); //裝配要傳遞的內容
+		rd.forward(req, resp);
 		
 	}
-
-	
 
 }
